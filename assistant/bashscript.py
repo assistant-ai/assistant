98645b2c556a50ea59b6f9ed4285921f3507597a
Gautam Kumar <gauta@amazon.com>
2023-04-18 16:49:43 -0700

    Adding bash command change

--- assistant/bashscript.py
+++ assistant/bashscript.py
@@ -1,12 +1,15 @@
 import os
 import openai 
 
+from . import const
+from . import utils
 
 
 def returnBashForGivenAction(text):
-    prompt = "Give me precise bash script to do following action " + text
-    openai.organization = os.getenv("OPENAI_ORGANIZATION_ID")
-    openai.api_key =  os.getenv("OPENAI_API_KEY")
+    prompt = "Give me precise bash script to do following action " + text + "  only return bash script"
+    openai.organization = const.GPT3_ORG_ID
+    openai.api_key = utils.get_gpt3_secret()
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
--- assistant/server.py
+++ assistant/server.py
@@ -1,6 +1,6 @@
 from wsgiref.simple_server import make_server
 
-from . import action, commands
+from . import action, bashscript 
 
 import abc
 import datetime
@@ -9,7 +9,7 @@ import requests
 
 
 
-class RephraseResource(abc.ABC):
+class ActionResource(abc.ABC):
 
     def on_post(self, req, resp):
         """Handles POST requests"""
@@ -25,14 +25,29 @@ class RephraseResource(abc.ABC):
         resp.media = {"result": isaction}
 
 
+class CommandResource(abc.ABC):
+
+    def on_post(self, req, resp):
+        """Handles POST requests"""
+        req_obj = req.get_media()
+        original_text = req_obj["text"]
+        if not original_text:
+            resp.status = falcon.HTTP_403
+            return
+
+        updated_text = bashscript.returnBashForGivenAction(original_text)
+        resp.status = falcon.HTTP_200
+        resp.media = {"command:": updated_text}
 
 
 
 app = falcon.App(cors_enable=True)
 
-is_action = RephraseResource()
+is_action = ActionResource()
+command_resource = CommandResource()
 
 app.add_route("/action", is_action)
+app.add_route("/command", command_resource)
 
 if __name__ == "__main__":
     with make_server("", 8080, app) as httpd:
